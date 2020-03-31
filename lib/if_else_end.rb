a71cce427f827d5f59f502002552b395f4d92b3b
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-31 20:02:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,9 @@
 # Write your solution here
 
 current_time = Time.now 
+current_time = current_time.to_i 
 
-
-def time_even_odd (current_time)
-  if current_time %2 == 0 
+if current_time %2 == 0 
   puts "Even!"
   else
   puts "Odd!"
